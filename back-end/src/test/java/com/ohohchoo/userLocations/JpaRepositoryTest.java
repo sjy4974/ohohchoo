e560f4f5e5e16a1b588419d8f7ccfcc864a3779a
johankkong <johankkong@gmail.com>
2022-12-16 23:44:17 +0900

    :white_check_mark: test : create saveTest code

---
+++ back-end/src/test/java/com/ohohchoo/userLocations/JpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.ohohchoo.userLocations;
+
+import com.ohohchoo.domain.userLocations.Entity.UserLocations;
+import com.ohohchoo.domain.userLocations.repository.UserLocationsRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * JpaRepository인 UserlocationsRepository에 대한
+ * test 파일입니다.
+ */
+
+@SpringBootTest
+@DisplayName("UserLocations Jpa Test")
+public class JpaRepositoryTest {
+
+    @Autowired
+    private UserLocationsRepository userLocationsRepository;
+
+    @Test @DisplayName("INSERT test")
+    void saveTestSuccess() {
+        UserLocations userLocations = UserLocations.builder()
+                .userId(1)
+                .locationCode(13)
+                .build();
+        userLocationsRepository.save(userLocations);
+    }
+
+}
